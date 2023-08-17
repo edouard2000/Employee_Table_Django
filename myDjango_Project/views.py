cef5f8b7c00c06a077de1be3e6215536c1e7152f
Edouard Kwizera <94765310+KwizeraEdouard@users.noreply.github.com>
2023-08-17 11:45:21 -0400

    my first commit

---
+++ myDjango_Project/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from employees.models import Employees
+
+def home(request):
+    employees = Employees.objects.all()
+    context = {
+        "employees": employees
+    }
+    return render(request, "home.html", context)
